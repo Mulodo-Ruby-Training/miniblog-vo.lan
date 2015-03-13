81791fdb35173ec0ca4172b2b0f90a8a9d96cce3
vo-lan-mulodo <vo.lan@mulodo.com>
2015-03-13 08:47:35 +0700

    update coding create edit delete post Signed-off-by: vo-lan-mulodo <vo.lan@mulodo.com>

--- src/ruby_miniblog/app/models/post.rb
+++ src/ruby_miniblog/app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/system/posts/images/default/:style/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
